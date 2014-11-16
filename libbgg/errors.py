79fdb5105f689c8a6ee4380cfdae5faa4d0b1861
Jay Deiman <jay@splitstreams.com>
2014-11-16 08:55:58 -0800

    Added APICallError

--- libbgg/errors.py
+++ libbgg/errors.py
@@ -1,10 +1,3 @@
 
 class InvalidInputError(Exception):
     pass
-
-class APICallError(Exception):
-
-    def __init__(self , msg , url):
-        Exception.__init__(self , msg)
-        self.url = url
-
